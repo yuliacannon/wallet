73603c991be28d305224b99307fcd0f93b054e61
yuliacannon <yuliapushka@gmail.com>
2020-04-27 18:52:51 +0300

    implemented business card class

--- Purse/Program.cs
+++ Purse/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessCardLibrary;
+using System;
 
 namespace Purse
 {
@@ -7,11 +8,22 @@ namespace Purse
         static void Main(string[] args)
         {
             Purse myPurse = new Purse();
+
+            myPurse.ShowAllBusinessCards();
         }
     }
 
     class Purse
     {
+        public BusinessCard card = new BusinessCard("Andrew Kowalski", "PLVision", "QA", "andrew.kovalski@plvision.eu");
 
+        public void ShowAllBusinessCards()
+        {
+            Console.WriteLine("Business cards in purse:");
+            foreach (BusinessCard card in new BusinessCard[] { card })
+            {
+                Console.WriteLine(card.ToString());
+            }
+        }
     }
 }
