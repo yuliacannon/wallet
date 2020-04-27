b100ffc631ec302d40cba47750191180bddf5c44
yuliacannon <yuliapushka@gmail.com>
2020-04-27 18:33:22 +0300

    main app init

---
+++ Purse/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Purse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Purse myPurse = new Purse();
+        }
+    }
+
+    class Purse
+    {
+
+    }
+}
